5efe906f37fad89e2fa4f66a4044ed255966076a
Ian Robertson <ian.b.robertson@gmail.com>
2008-05-19 20:30:44 +0000

    Implement and test DefaultPropertyFormatter.  For now, at least, drop the appendable approach, at the expense of creating a few more StringBuilders than necessary. Notably, we'll create a new one every time we call toString on a sub-object, so this may not be so bad; it really only affects arrays and other containers.
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@8 7fc7bed9-ed58-45a2-beed-3cb072bc7445

---
+++ Pojomatic/src/test/java/org/pojomatic/formatter/DefaultPropertyFormatterTest.java
@@ -0,0 +1,73 @@
+package org.pojomatic.formatter;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultPropertyFormatterTest {
+  private PropertyFormatter formatter;
+
+  @Before public void setUp() {
+    formatter = new DefaultPropertyFormatter();
+    formatter.initialize(null);
+  }
+
+  @Test public void testFormat() {
+    assertEquals("7", formatter.format(7));
+  }
+
+  @Test public void testFormatNull() {
+    assertEquals("null", formatter.format(null));
+  }
+
+  @Test public void testFormatList() {
+    assertEquals("[5, 7]", formatter.format(Arrays.asList(5, 7)));
+  }
+
+  @Test public void testFormatArrayOfObjects() {
+    assertEquals("[5, 7]", formatter.format(new Integer[] {5, 7}));
+  }
+
+  @Test public void testFormatArrayOfBooleans() {
+    assertEquals("[true, false]", formatter.format(new boolean[] {true, false}));
+  }
+
+  @Test public void testFormatArrayOfBytes() {
+    assertEquals("[5, 7]", formatter.format(new byte[] {5, 7}));
+  }
+
+  @Test public void testFormatArrayOfChars() {
+    assertEquals("['0x5', 'b']", formatter.format(new char[] {5, 'b'}));
+  }
+
+  @Test public void testFormatArrayOfInts() {
+    assertEquals("[5, 7]", formatter.format(new int[] {5, 7}));
+  }
+
+  @Test public void testFormatArrayOfLongs() {
+    assertEquals("[5, 7]", formatter.format(new long[] {5, 7}));
+  }
+
+  @Test public void testFormatArrayOfFloats() {
+    assertEquals("[5.0, 7.0]", formatter.format(new float[] {5, 7}));
+  }
+
+  @Test public void testFormatArrayOfDoubles() {
+    assertEquals("[5.0, 7.0]", formatter.format(new double[] {5, 7}));
+  }
+
+  @Test public void testFormatDoubleArray() {
+    assertEquals(
+      "[[1, 2], [3, 4]]",
+      formatter.format(new Integer[][] {new Integer[] { 1, 2 }, new Integer[] {3, 4} }));
+  }
+
+  @Test public void testFormatDoubleArrayOfPrimatives() {
+    assertEquals(
+      "[[1, 2], [3, 4]]",
+      formatter.format(new int[][] {new int[] { 1, 2 }, new int[] {3, 4} }));
+  }
+}
