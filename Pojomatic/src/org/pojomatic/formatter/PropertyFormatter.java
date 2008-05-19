f66f2d5862795bf5b6ba8d5cadb324fbef42547d
Ian Robertson <ian.b.robertson@gmail.com>
2008-05-19 19:25:58 +0000

    Javadoc
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@4 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/org/pojomatic/formatter/PropertyFormatter.java
+++ Pojomatic/src/org/pojomatic/formatter/PropertyFormatter.java
@@ -2,8 +2,23 @@ package org.pojomatic.formatter;
 
 import java.lang.reflect.AnnotatedElement;
 
+/**
+ * A formatter for a property.
+ * Any implementation of {@code PropertyFormatter} must have a public no-argument constructor.
+ */
 public interface PropertyFormatter {
+  /**
+   * Initialize the formatter for use; this method will be called exactly once on an instance, prior
+   * to any calls to {@link #format(Object, Appendable)}.  This method does not need to be
+   * thread-safe.
+   * @param element
+   */
   public void initialize(AnnotatedElement element);
 
+  /**
+   * Format a given value.  This method must be thread safe.
+   * @param value the value to format
+   * @param appendable where to format the value to
+   */
   public void format(Object value, Appendable appendable);
 }
