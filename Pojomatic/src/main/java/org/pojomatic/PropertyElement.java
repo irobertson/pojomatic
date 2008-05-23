dfc09800d97829beb57190ae70a8c5802740f41a
Chris Hansen <hansen.chris.w@gmail.com>
2008-05-23 23:56:59 +0000

    Initial implementation of PropertyElement
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@11 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/main/java/org/pojomatic/PropertyElement.java
+++ Pojomatic/src/main/java/org/pojomatic/PropertyElement.java
@@ -1,20 +1,61 @@
 package org.pojomatic;
 
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-public class PropertyElement<T> {
-  private AnnotatedElement element;
+public final class PropertyElement<T> {
+  private final AnnotatedElement element;
+
+  /**
+   * A {@code PropertyElement} can either be a field or a method
+   */
+  private final boolean isField;
+
+  public PropertyElement(Field propertyField) {
+    this(propertyField, true);
+  }
+
+  public PropertyElement(Method propertyAccessor) {
+    this(propertyAccessor, false);
+  }
+
+  private PropertyElement(AnnotatedElement element, boolean isField) {
+    this.element = element;
+    this.isField = isField;
+  }
 
   public Object getValue(T instance) {
-    //TODO get the value on demand
-    return null;
+    if (instance == null) {
+      throw new NullPointerException("Instance is null: cannot get property value");
+    }
+
+    if (isField) {
+      Field field = (Field)getElement();
+      field.setAccessible(true);
+      try {
+        return field.get(instance);
+      } catch (IllegalArgumentException e) {
+        throw new RuntimeException(e);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      Method method = (Method)getElement();
+      try {
+        return method.invoke(instance, (Object[])null);
+      } catch (IllegalArgumentException e) {
+        throw new RuntimeException(e);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      } catch (InvocationTargetException e) {
+        throw new RuntimeException(e.getCause());
+      }
+    }
   }
 
   public AnnotatedElement getElement() {
     return this.element;
-  }
-
-  public void setElement(AnnotatedElement element) {
-    this.element = element;
   }
 }
---
+++ Pojomatic/src/test/java/org/pojomatic/PropertyElementTest.java
@@ -0,0 +1,50 @@
+package org.pojomatic;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+public class PropertyElementTest {
+
+  @Test(expected=NullPointerException.class)
+  public void testGetValueNullField() throws Exception {
+    PropertyElement<PropertyElementTest> propertyElement =
+      new PropertyElement<PropertyElementTest>(getTestField());
+    propertyElement.getValue(null);
+  }
+
+  @Test(expected=NullPointerException.class)
+  public void testGetValueNullMethod() throws Exception {
+    PropertyElement<PropertyElementTest> propertyElement =
+      new PropertyElement<PropertyElementTest>(getTestMethod());
+    propertyElement.getValue(null);
+  }
+
+  @Test
+  public void testGetValueField() throws Exception {
+    PropertyElement<PropertyElementTest> propertyElement =
+      new PropertyElement<PropertyElementTest>(getTestField());
+    assertEquals(testField, propertyElement.getValue(this));
+  }
+
+  @Test
+  public void testGetValueMethod() throws Exception {
+    PropertyElement<PropertyElementTest> propertyElement =
+      new PropertyElement<PropertyElementTest>(getTestMethod());
+    assertEquals(testAccessor(), propertyElement.getValue(this));
+  }
+
+  private Method getTestMethod() throws Exception {
+    return this.getClass().getMethod("testAccessor", (Class[])null);
+  }
+
+  private Field getTestField() throws Exception {
+    return this.getClass().getDeclaredField("testField");
+  }
+
+  public Object testAccessor() { return "Test string"; }
+  private final Object testField = new Object();
+}
