9b701c505cdcdc04053425656d2bacbbc839655a
Ian Robertson <ian.b.robertson@gmail.com>
2008-06-09 21:11:15 +0000

    Stubs for the next task
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@19 7fc7bed9-ed58-45a2-beed-3cb072bc7445

---
+++ Pojomatic/src/main/java/org/pojomatic/internal/ClassProperties.java
@@ -0,0 +1,21 @@
+package org.pojomatic.internal;
+
+import org.pojomatic.PropertyElement;
+
+public class ClassProperties {
+  public ClassProperties(Class<?> pojoClass) {
+
+  }
+
+  public Iterable<PropertyElement<?>> getEqualsProperties() {
+    return null;
+  }
+
+  public Iterable<PropertyElement<?>> getHashCodeProperties() {
+    return null;
+  }
+
+  public Iterable<PropertyElement<?>> getToStringProperties() {
+    return null;
+  }
+}
---
+++ Pojomatic/src/test/java/org/pojomatic/internal/ClassPropertiesTest.java
@@ -0,0 +1,21 @@
+package org.pojomatic.internal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClassPropertiesTest {
+
+  @Test public void testGetEqualsProperties() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test public void testGetHashCodeProperties() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test public void testGetToStringProperties() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
