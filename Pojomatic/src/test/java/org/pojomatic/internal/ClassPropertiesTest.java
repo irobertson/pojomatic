10d7f0932b0237ebacc844ba9610f786deefd61d
Ian Robertson <ian.b.robertson@gmail.com>
2008-06-16 20:30:44 +0000

    Beef up tests
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@22 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/test/java/org/pojomatic/internal/ClassPropertiesTest.java
+++ Pojomatic/src/test/java/org/pojomatic/internal/ClassPropertiesTest.java
@@ -2,12 +2,16 @@ package org.pojomatic.internal;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
 import org.pojomatic.PropertyElement;
 import org.pojomatic.PropertyField;
+import org.pojomatic.annotations.AutoProperty;
+import org.pojomatic.annotations.PojomaticDefaultPolicy;
+import org.pojomatic.annotations.PojomaticPolicy;
 import org.pojomatic.annotations.Property;
 
 public class ClassPropertiesTest {
@@ -15,14 +19,34 @@ public class ClassPropertiesTest {
 
   @Test
   public void testSimpleFields() throws Exception {
+    final PropertyElement privateStringField = field(FieldPojo.class, "privateString");
+    final PropertyElement publicIntField = field(FieldPojo.class, "publicInt");
+    final PropertyElement onlyForStringField = field(FieldPojo.class, "onlyForToString");
+
     ClassProperties classProperties = ClassProperties.createInstance(FieldPojo.class);
-    Set<PropertyElement> expected =
-      asSet(field(FieldPojo.class, "privateString"), field(FieldPojo.class, "publicInt"));
-    assertEquals(expected, new HashSet<PropertyElement>(classProperties.getEqualsProperties()));
-    assertEquals(expected, new HashSet<PropertyElement>(classProperties.getHashCodeProperties()));
-    assertEquals(expected, new HashSet<PropertyElement>(classProperties.getToStringProperties()));
+
+    Set<PropertyElement> expected = asSet(privateStringField, publicIntField);
+    assertEquals(expected, asSet(classProperties.getEqualsProperties()));
+    assertEquals(expected, asSet(classProperties.getHashCodeProperties()));
+    assertEquals(
+      asSet(privateStringField, publicIntField, onlyForStringField),
+      asSet(classProperties.getToStringProperties()));
   }
 
+  @Test
+  public void testAutoFields() throws Exception {
+
+    final PropertyElement stringField = field(AutoFieldPojo.class, "string");
+    final PropertyElement allInDoubleField = field(AutoFieldPojo.class, "allInDouble");
+
+    ClassProperties classProperties = ClassProperties.createInstance(AutoFieldPojo.class);
+
+    assertEquals(asSet(allInDoubleField), asSet(classProperties.getEqualsProperties()));
+    assertEquals(asSet(allInDoubleField), asSet(classProperties.getHashCodeProperties()));
+    assertEquals(
+      asSet(stringField, allInDoubleField),
+      asSet(classProperties.getToStringProperties()));
+  }
 
   private static Set<PropertyElement> asSet(PropertyElement... elements) {
     HashSet<PropertyElement> result = new HashSet<PropertyElement>();
@@ -30,6 +54,10 @@ public class ClassPropertiesTest {
       result.add(element);
     }
     return result;
+  }
+
+  private static Set<PropertyElement> asSet(Collection<PropertyElement> elements) {
+    return new HashSet<PropertyElement>(elements);
   }
 
   private static PropertyElement field(Class<?> clazz, String fieldName) throws Exception {
@@ -43,7 +71,21 @@ public class ClassPropertiesTest {
 
     @Property
     public int publicInt;
+
+    @SuppressWarnings("unused")
+    @Property(policy=PojomaticPolicy.TO_STRING)
+    private int onlyForToString;
   }
 
+  @AutoProperty(policy=PojomaticDefaultPolicy.TO_STRING)
+  public static class AutoFieldPojo {
+    public String string;
+
+    @Property(policy=PojomaticPolicy.NONE)
+    public int ignoredInt;
+
+    @Property(policy=PojomaticPolicy.ALL)
+    public double allInDouble;
+  }
 
 }
