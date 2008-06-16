f119caf1fe6711aa9ba17ccd4621f47970fe15fe
Ian Robertson <ian.b.robertson@gmail.com>
2008-06-16 21:28:03 +0000

    Add method support.  Next stop: inheritance
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@23 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/main/java/org/pojomatic/internal/ClassProperties.java
+++ Pojomatic/src/main/java/org/pojomatic/internal/ClassProperties.java
@@ -1,11 +1,13 @@
 package org.pojomatic.internal;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.Map;
 
+import org.pojomatic.PropertyAccessor;
 import org.pojomatic.PropertyElement;
 import org.pojomatic.PropertyField;
 import org.pojomatic.annotations.AutoProperty;
@@ -39,6 +41,40 @@ public class ClassProperties {
         properties.get(role).add(new PropertyField(field));
       }
     }
+
+    for (Method method : pojoClass.getDeclaredMethods()) {
+      Property property = method.getAnnotation(Property.class);
+      PojomaticPolicy propertyPolicy = null;
+      if (property != null) {
+        if (!methodSignatureIsAccessor(method)) {
+          throw new IllegalArgumentException(
+            "Method " + method +
+            " is annotated with @Property but either takes arguments or returns void");
+        }
+        propertyPolicy = property.policy();
+      }
+      else if (!methodIsAccessor(method)) {
+        continue;
+      }
+      for (PropertyRole role : PropertyFilter.getRoles(propertyPolicy, classPolicy)) {
+        properties.get(role).add(new PropertyAccessor(method));
+      }
+    }
+  }
+
+  private static boolean methodIsAccessor(Method method) {
+    return methodSignatureIsAccessor(method)
+      && isAccessorName(method.getName());
+  }
+
+  private static boolean methodSignatureIsAccessor(Method method) {
+    return ! Void.TYPE.equals(method.getReturnType())
+      && method.getParameterTypes().length == 0;
+  }
+
+  private static boolean isAccessorName(String name) {
+    //FIXME - compile
+    return name.matches("(get|is)\\P{Ll}.*");
   }
 
   public Collection<PropertyElement> getEqualsProperties() {
--- Pojomatic/src/test/java/org/pojomatic/internal/ClassPropertiesTest.java
+++ Pojomatic/src/test/java/org/pojomatic/internal/ClassPropertiesTest.java
@@ -1,12 +1,13 @@
 package org.pojomatic.internal;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
+import org.pojomatic.PropertyAccessor;
 import org.pojomatic.PropertyElement;
 import org.pojomatic.PropertyField;
 import org.pojomatic.annotations.AutoProperty;
@@ -48,20 +49,59 @@ public class ClassPropertiesTest {
       asSet(classProperties.getToStringProperties()));
   }
 
-  private static Set<PropertyElement> asSet(PropertyElement... elements) {
-    HashSet<PropertyElement> result = new HashSet<PropertyElement>();
-    for (PropertyElement element : elements) {
-      result.add(element);
-    }
-    return result;
+  @Test
+  public void testSimpleMethods() throws Exception {
+    final PropertyElement getIntMethod = method(MethodPojo.class, "getInt");
+    final PropertyElement privateStringMethod = method(MethodPojo.class, "privateString");
+    final PropertyElement onlyForEqualsMethod = method(MethodPojo.class, "onlyForEquals");
+
+    ClassProperties classProperties = ClassProperties.createInstance(MethodPojo.class);
+
+    assertEquals(
+      asSet(getIntMethod, privateStringMethod, onlyForEqualsMethod),
+      asSet(classProperties.getEqualsProperties()));
+    assertEquals(
+      asSet(getIntMethod, privateStringMethod),
+      asSet(classProperties.getHashCodeProperties()));
+    assertEquals(
+      asSet(getIntMethod, privateStringMethod),
+      asSet(classProperties.getToStringProperties()));
   }
 
-  private static Set<PropertyElement> asSet(Collection<PropertyElement> elements) {
-    return new HashSet<PropertyElement>(elements);
+  @Test
+  public void testAutoMethods() throws Exception {
+    final Set<PropertyElement> commonProperties = asSet(
+      method(AutoMethodPojo.class, "getInt"),
+      method(AutoMethodPojo.class, "isBoolean"),
+      method(AutoMethodPojo.class, "is_boolean"),
+      method(AutoMethodPojo.class, "get_int"));
+    final Set<PropertyElement> equalsHashCodeProperties =
+      new HashSet<PropertyElement>(commonProperties);
+    equalsHashCodeProperties.add(method(AutoMethodPojo.class, "getHashCodeAndEquals"));
+
+    ClassProperties classProperties = ClassProperties.createInstance(AutoMethodPojo.class);
+
+    assertEquals(equalsHashCodeProperties, asSet(classProperties.getEqualsProperties()));
+    assertEquals(equalsHashCodeProperties, asSet(classProperties.getHashCodeProperties()));
+    assertEquals(commonProperties, asSet(classProperties.getToStringProperties()));
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testAnnotatedMethodReturningVoid() {
+      ClassProperties.createInstance(MethodReturnsVoidPojo.class);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testAnnotatedMethodTakingArgs() {
+      ClassProperties.createInstance(MethodTakesArgsPojo.class);
   }
 
   private static PropertyElement field(Class<?> clazz, String fieldName) throws Exception {
     return new PropertyField(clazz.getDeclaredField(fieldName));
+  }
+
+  private static PropertyElement method(Class<?> clazz, String methodName) throws Exception {
+    return new PropertyAccessor(clazz.getDeclaredMethod(methodName));
   }
 
   public static class FieldPojo {
@@ -88,4 +128,62 @@ public class ClassPropertiesTest {
     public double allInDouble;
   }
 
+  public static class MethodPojo {
+    @Property
+    public int getInt() { return 0; }
+
+    @Property
+    @SuppressWarnings("unused")
+    private String privateString() { return null; }
+
+    @Property(policy=PojomaticPolicy.EQUALS)
+    public double onlyForEquals() { return 0.0; }
+  }
+
+  @AutoProperty(policy=PojomaticDefaultPolicy.ALL)
+  public static class AutoMethodPojo {
+    @Property(policy=PojomaticPolicy.NONE)
+    public String getIgnored() { return null; }
+
+    @Property(policy=PojomaticPolicy.HASHCODE_EQUALS)
+    public double getHashCodeAndEquals() { return 0.0; }
+
+    // getter variants
+    public int getInt() { return 0; }
+    public boolean isBoolean() { return true; }
+    public boolean is_boolean() { return true; }
+    public int get_int() { return 0; }
+
+    // not getters
+    public boolean isaEnabled() { return true; }
+    public int gettyIsAMueseum() { return 1; }
+    public String thisIsNotAGetter() { return "really, it's not"; }
+
+    // some methods we should not grab
+    public void getHello() {}
+    public int getTripple(int arg) { return arg * 3; }
+  }
+
+  public static class MethodReturnsVoidPojo {
+    @Property
+    public void noReturn() {}
+  }
+
+  public static class MethodTakesArgsPojo {
+    @Property
+    public int takesArgs(String death) { return death.length(); }
+  }
+
+  private static Set<PropertyElement> asSet(PropertyElement... elements) {
+    HashSet<PropertyElement> result = new HashSet<PropertyElement>();
+    for (PropertyElement element : elements) {
+      result.add(element);
+    }
+    return result;
+  }
+
+  private static Set<PropertyElement> asSet(Collection<PropertyElement> elements) {
+    return new HashSet<PropertyElement>(elements);
+  }
 }
+
