a800a0ecd8610470b9a919d8cc60964d43ee26fe
Ian Robertson <ian.b.robertson@gmail.com>
2008-11-10 22:02:30 +0000

    Maven documentation
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@71 7fc7bed9-ed58-45a2-beed-3cb072bc7445

---
+++ Pojomatic/src/test/java/examples/Person.java
@@ -0,0 +1,41 @@
+package examples;
+
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
+
+@AutoProperty
+public class Person {
+  private final String firstName;
+  private final String lastName;
+  private final int age;
+
+  public Person(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  @Override public int hashCode() {
+    return Pojomatic.hashCode(this);
+  }
+
+  @Override public String toString() {
+    return Pojomatic.toString(this);
+  }
+
+  @Override public boolean equals(Object o) {
+    return Pojomatic.equals(this, o);
+  }
+}
---
+++ Pojomatic/src/test/java/examples/PersonMain.java
@@ -0,0 +1,7 @@
+package examples;
+
+public class PersonMain {
+  public static void main(String[] args) {
+    System.out.println(new Person("John", "Doe", 32).toString());
+  }
+}
