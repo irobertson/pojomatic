38e224cab9aae42fd0e8f355d81dcfa6fdde0cb3
Ian Robertson <ian.b.robertson@gmail.com>
2009-01-26 22:03:58 +0000

    Implement equals and hashCode (eat our own dogfood)
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@91 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/main/java/org/pojomatic/diff/PropertyDifferences.java
+++ Pojomatic/src/main/java/org/pojomatic/diff/PropertyDifferences.java
@@ -3,7 +3,11 @@ package org.pojomatic.diff;
 import java.util.Collections;
 import java.util.List;
 
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.Property;
+
 public class PropertyDifferences implements Differences {
+  @Property
   private final List<Difference> differences;
 
   public PropertyDifferences(List<Difference> differences) {
@@ -21,6 +25,9 @@ public class PropertyDifferences implements Differences {
   public String toString() {
     return differences.isEmpty() ? "no differences" : differences.toString();
   }
+
+  @Override public boolean equals(Object other) { return Pojomatic.equals(this, other); }
+  @Override public int hashCode() { return Pojomatic.hashCode(this); }
 
   public boolean areEqual() {
     return differences.isEmpty();
