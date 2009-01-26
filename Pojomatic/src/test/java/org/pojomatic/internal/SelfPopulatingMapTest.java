769587df483853184d3ef38a33af9e4df0b52cb4
Ian Robertson <ian.b.robertson@gmail.com>
2009-01-26 22:02:07 +0000

    modesty: increase coverage
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@90 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/test/java/org/pojomatic/internal/SelfPopulatingMapTest.java
+++ Pojomatic/src/test/java/org/pojomatic/internal/SelfPopulatingMapTest.java
@@ -2,6 +2,8 @@ package org.pojomatic.internal;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.Test;
 
 public class SelfPopulatingMapTest {
@@ -39,5 +41,44 @@ public class SelfPopulatingMapTest {
       t.join();
     }
     assertSame(results[0], results[1]);
+  }
+
+  @Test
+  public void testBadConstructionFirstTime() throws Exception {
+    final AtomicBoolean firstTime = new AtomicBoolean(false);
+    final SelfPopulatingMap<String, String> selfPopulatingMap =
+      new SelfPopulatingMap<String, String>() {
+      @Override protected String create(String key) {
+        try {
+          Thread.sleep(10); // ensure that two threads have time to collide.
+        }
+        catch (InterruptedException e) {}
+        if (firstTime.getAndSet(true)) {
+          return new String(key);
+        }
+        else {
+          throw new RuntimeException("failing on first attempt");
+        }
+      }
+    };
+    int numThreads = 2;
+    Thread[] threads = new Thread[numThreads];
+    final String[] results = new String[numThreads];
+    for (int i = 0; i < threads.length; i++) {
+      final int threadNumber = i;
+      threads[i] = new Thread() {
+        @Override public void run() {
+          results[threadNumber] = selfPopulatingMap.get("x");
+        }
+      };
+    }
+    for (Thread t: threads) {
+      t.start();
+    }
+    for (Thread t: threads) {
+      t.join();
+    }
+    assertNull(results[0]);
+    assertEquals("x", results[1]);
   }
 }
