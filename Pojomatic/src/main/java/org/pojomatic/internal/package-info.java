38a4c92123f1c5172751ab12d4e9a72f31e3b4a7
Chris Hansen <hansen.chris.w@gmail.com>
2009-01-29 17:58:18 +0000

    Document that the internal package is intended for internal use only.
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@93 7fc7bed9-ed58-45a2-beed-3cb072bc7445

---
+++ Pojomatic/src/main/java/org/pojomatic/internal/package-info.java
@@ -0,0 +1,6 @@
+
+/**
+ * The classes in this package are intended for internal use only. Additionally, these
+ * implementations are subject to change in future versions.
+ */
+package org.pojomatic.internal;
