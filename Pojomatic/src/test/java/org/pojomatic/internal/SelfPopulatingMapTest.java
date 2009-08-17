ce15e53a1aacee256af9196493cfaab1761b1f36
Ian Robertson <ian.b.robertson@gmail.com>
2009-08-17 17:49:13 +0000

    No need for this test to be multithreaded
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@131 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- Pojomatic/src/test/java/org/pojomatic/internal/SelfPopulatingMapTest.java
+++ Pojomatic/src/test/java/org/pojomatic/internal/SelfPopulatingMapTest.java
@@ -49,37 +49,23 @@ public class SelfPopulatingMapTest {
     final SelfPopulatingMap<String, String> selfPopulatingMap =
       new SelfPopulatingMap<String, String>() {
       @Override protected String create(String key) {
-        try {
-          Thread.sleep(10); // ensure that two threads have time to collide.
-        }
-        catch (InterruptedException e) {}
         if (firstTime.getAndSet(true)) {
           return new String(key);
         }
         else {
-          //first time through, throw an exception
-          throw new RuntimeException("This is expected: failing on first attempt");
+          throw new RuntimeException("first");
         }
       }
     };
-    int numThreads = 2;
-    Thread[] threads = new Thread[numThreads];
-    final String[] results = new String[numThreads];
-    for (int i = 0; i < threads.length; i++) {
-      final int threadNumber = i;
-      threads[i] = new Thread() {
-        @Override public void run() {
-          results[threadNumber] = selfPopulatingMap.get("x");
-        }
-      };
+
+    try {
+      selfPopulatingMap.get("x");
+      fail("Exception expected");
     }
-    for (Thread t: threads) {
-      t.start();
+    catch(RuntimeException e) {
+      assertEquals("first", e.getMessage());
     }
-    for (Thread t: threads) {
-      t.join();
-    }
-    assertNull(results[0]);
-    assertEquals("x", results[1]);
+
+    assertEquals("x", selfPopulatingMap.get("x"));
   }
 }
