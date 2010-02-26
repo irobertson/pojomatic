464fab3197c2c4f69bbc1cfc69ada3cb5f4e31de
Chris Hansen <hansen.chris.w@gmail.com>
2010-02-26 06:43:13 +0000

    Rename test as well.
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@194 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- PojomaticAll/Pojomatic/src/test/java/org/pojomatic/diff/ValueDifferenceTest.java
+++ PojomaticAll/Pojomatic/src/test/java/org/pojomatic/diff/ValueDifferenceTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class DifferenceTest {
+public class ValueDifferenceTest {
   private ValueDifference DIFFERENCE = new ValueDifference("foo", "this", "that");
 
   @Test
