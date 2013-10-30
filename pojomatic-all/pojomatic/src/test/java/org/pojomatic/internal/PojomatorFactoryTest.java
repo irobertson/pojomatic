65ea99d7852d1fa1d55d4b1d7fc7e5a9f4ff5338
Ian Robertson <ian.b.robertson@gmail.com>
2013-10-30 19:04:44 +0000

    import cleaning
    git-svn-id: svn://svn.code.sf.net/p/pojomatic/code/trunk@253 7fc7bed9-ed58-45a2-beed-3cb072bc7445

--- pojomatic-all/pojomatic/src/test/java/org/pojomatic/internal/PojomatorFactoryTest.java
+++ pojomatic-all/pojomatic/src/test/java/org/pojomatic/internal/PojomatorFactoryTest.java
@@ -1,12 +1,11 @@
 package org.pojomatic.internal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.IOException;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
-import java.security.cert.Certificate;
 import java.util.Arrays;
 
 import org.junit.Test;
