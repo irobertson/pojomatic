936536983562eceabe13c8792b960d36fad6cea1
Ian Robertson <ian.b.robertson@gmail.com>
2014-02-13 16:30:35 -0700

    Experiment with a few different approaches to data-driven testing (using TestNG).

--- pojomatic-all/pojomatic/src/test/java/org/pojomatic/internal/MatrixTest.java
+++ pojomatic-all/pojomatic/src/test/java/org/pojomatic/internal/MatrixTest.java
@@ -1,22 +1,63 @@
 package org.pojomatic.internal;
 
-import static org.junit.Assert.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import org.junit.Test;
 import org.pojomatic.internal.factory.PojoDescriptor;
 import org.pojomatic.internal.factory.PojoFactory;
 import org.pojomatic.internal.factory.PropertyDescriptor;
 
 public class MatrixTest {
 
+  @DataProvider(name = "types")
+  public static Object[][] types() {
+    Object[][] result = new Object[Type.values().length][1];
+    int i = 0;
+    for (Type type: Type.values()) {
+      result[i++][0] = type;
+    }
+    return result;
+  }
+
+  @DataProvider(name = "typesAndValues")
+  public static Object[][] typesAndValues() {
+    List<Object[]> result = new ArrayList<>();
+    for (Type type: Type.values()) {
+      for (Object value: type.sampleValues) {
+        result.add(new Object[] { type, value });
+      }
+    }
+    return result.toArray(new Object[0][0]);
+  }
+
+  @Test(dataProvider = "types")
+  public void testHashCode1(Type type) {
+    PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
+    for (Object value: type.sampleValues) {
+      AssertJUnit.assertEquals(
+        "value: " + value,
+        31 + Objects.hashCode(value), pojoFactory.pojomator().doHashCode(pojoFactory.create(value)));
+    }
+  }
+
+  @Test(dataProvider = "typesAndValues")
+  public void testHashCode2(Type type, Object value) {
+    PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
+    AssertJUnit.assertEquals(
+      31 + Objects.hashCode(value), pojoFactory.pojomator().doHashCode(pojoFactory.create(value)));
+  }
+
   @Test
   public void testHashCode() {
     for (Type type: Type.values()) {
       PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
       for (Object value: type.sampleValues) {
-        assertEquals(
+        AssertJUnit.assertEquals(
           "type: " + type.clazz + ", value: " + value,
           31 + Objects.hashCode(value), pojoFactory.pojomator().doHashCode(pojoFactory.create(value)));
       }
@@ -28,11 +69,28 @@ public class MatrixTest {
     for (Type type: Type.values()) {
       PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
       for (Object value: type.sampleValues) {
-        assertEquals(
+        AssertJUnit.assertEquals(
           "type: " + type.clazz + ", value: " + value,
           "Pojo{x: {" + Objects.toString(value) + "}}", pojoFactory.pojomator().doToString(pojoFactory.create(value)));
       }
     }
+  }
+
+  @Test(dataProvider = "types")
+  public void testToString1(Type type) {
+    PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
+    for (Object value: type.sampleValues) {
+      AssertJUnit.assertEquals(
+        "value: " + value,
+        "Pojo{x: {" + Objects.toString(value) + "}}", pojoFactory.pojomator().doToString(pojoFactory.create(value)));
+    }
+  }
+
+  @Test(dataProvider = "typesAndValues")
+  public void testToString2(Type type, Object value) {
+    PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
+    AssertJUnit.assertEquals(
+      "Pojo{x: {" + Objects.toString(value) + "}}", pojoFactory.pojomator().doToString(pojoFactory.create(value)));
   }
 
   @Test
@@ -41,16 +99,46 @@ public class MatrixTest {
       PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
       for (Object value1: type.sampleValues) {
         for (Object value2: type.sampleValues) {
-          assertEquals(
+          AssertJUnit.assertEquals(
             "type: " + type.clazz + ", value1: " + value1 + ", value2: " + value2,
             value1 == value2,
             pojoFactory.pojomator().doEquals(pojoFactory.create(value1), pojoFactory.create(value2)));
         }
-        assertFalse(
+        AssertJUnit.assertFalse(
           "type: " + type.clazz + ", value1: " + value1,
           pojoFactory.pojomator().doEquals(pojoFactory.create(value1), null));
       }
     }
+  }
+
+  @Test(dataProvider = "types")
+  public void testEquals1(Type type) {
+    PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
+    for (Object value1: type.sampleValues) {
+      for (Object value2: type.sampleValues) {
+        AssertJUnit.assertEquals(
+          "value1: " + value1 + ", value2: " + value2,
+          value1 == value2,
+          pojoFactory.pojomator().doEquals(pojoFactory.create(value1), pojoFactory.create(value2)));
+      }
+      AssertJUnit.assertFalse(
+        "type: " + type.clazz + ", value1: " + value1,
+        pojoFactory.pojomator().doEquals(pojoFactory.create(value1), null));
+    }
+  }
+
+  @Test(dataProvider = "typesAndValues")
+  public void testEquals2(Type type, Object value1) {
+    PojoFactory pojoFactory = new PojoFactory(new PojoDescriptor(new PropertyDescriptor(type.clazz)));
+    for (Object value2: type.sampleValues) {
+      AssertJUnit.assertEquals(
+        "value2: " + value2,
+        value1 == value2,
+        pojoFactory.pojomator().doEquals(pojoFactory.create(value1), pojoFactory.create(value2)));
+    }
+    AssertJUnit.assertFalse(
+      "type: " + type.clazz + ", value1: " + value1,
+      pojoFactory.pojomator().doEquals(pojoFactory.create(value1), null));
   }
 
 }
