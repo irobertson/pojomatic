59b7ef48a74bf541bcda232c23ff9a0a66b66bfe
Ian Robertson <ian.b.robertson@gmail.com>
2014-03-16 21:25:56 -0600

    Javadoc - fill in @return clauses

--- pojomatic-all/pojomatic/src/test/java/org/pojomatic/internal/Type.java
+++ pojomatic-all/pojomatic/src/test/java/org/pojomatic/internal/Type.java
@@ -22,28 +22,28 @@ public interface Type {
   /**
    * Compute the hashCode for a value of this type. Do not recursively process arrays
    * @param value
-   * @return
+   * @return the non-recursive hashCode for {@code value}.
    */
   int hashCode(Object value);
 
   /**
    * Compute the hashCode for a value of this type, recursing into arrays as needed.
    * @param value
-   * @return
+   * @return recursive hashCode for {@code value}
    */
   int deepHashCode(Object value);
 
   /**
    * Compute the desired toString representation for a value of this type.
    * @param value
-   * @return
+   * @return the desired non-recursive toString representation for {@code value}.
    */
   String toString(Object value);
 
   /**
    * Compute the desired toString representation for a value of this type, recursing into arrays as needed.
    * @param value
-   * @return
+   * @return the desired recursive toString representation for {@code value}.
    */
   String deepToString(Object value);
 }
